c0cf3ce834e5bc90ddf947879a5d5f367d68a579
Andrei-Predoiu <andrei.vn92@gmail.com>
2014-04-30 15:00:23 +0200

    updated natives

--- interrupts.c
+++ interrupts.c
@@ -5,14 +5,8 @@
 
 int16 vm_InterruptDispatcher_interrupt(int32 *fp, int8 n);
 int32 isrMethodStack[128];
-
-int16 n_AtmelProject_LEDBlink_InterruptTest_setupInterrupts(){
-	 sei();
-	return -1;
-}
-
+ 
 ISR(TIMER1_COMPA_vect)
 {
-	//PORTH = PORTH ^ _BV(PH1);
 	vm_InterruptDispatcher_interrupt(&isrMethodStack[0],17);
 }
